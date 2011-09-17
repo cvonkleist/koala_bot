82ea9b67cb88dadfb8e3d7d23732281ae9d6bdef
Brian Lewis <brian@lorf.org>
2011-09-17 19:31:34 -0400

    misc cleanup

--- koala_bot.rb
+++ koala_bot.rb
@@ -1,5 +1,5 @@
 require "dependencies"
-require 'net/yail'
+require "net/yail"
 
 server_host = ARGV.shift
 server_port = (ARGV.shift || 6667).to_i
@@ -13,16 +13,16 @@ end
 channels = []
 
 irc = Net::YAIL.new(
-  :address    => server_host,
-  :port => server_port,
-  :username   => 'KoalaBot',
-  :realname   => 'Koala Bot',
-  :nicknames  => ['koala_bot', 'koala_bot_'],
-  :loud => true
+  :address   => server_host,
+  :port      => server_port,
+  :username  => "KoalaBot",
+  :realname  => "Koala Bot",
+  :nicknames => ["koala_bot", "koala_bot_"],
+  :loud      => true
 )
 
 irc.on_welcome do |event|
-  irc.join('#skillhouse')
+  irc.join("#skillhouse")
 end
 
 irc.on_join do |event|
