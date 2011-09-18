f640f723ed85adcfee552f6f47120508c82aee72
Brian Lewis <brian@lorf.org>
2011-09-17 20:01:35 -0400

    use array cycler instead of picking lines randomly

--- koala_bot.rb
+++ koala_bot.rb
@@ -1,14 +1,25 @@
 require "dependencies"
 require "net/yail"
 
+class Cycler
+  def initialize(elements)
+    raise "elements empty" if elements.empty?
+    @elements = elements
+    @idx_max  = elements.length - 1
+    @idx      = 0
+  end
+
+  def next
+    r = @elements[@idx]
+    @idx = (@idx == @idx_max) ? 0 : (@idx + 1)
+    r
+  end
+end
+
 server_host = ARGV.shift
 server_port = (ARGV.shift || 6667).to_i
 
-$koala_lines = File.read("koalas.txt").split("\n")
-
-def random_line
-  $koala_lines[rand($koala_lines.count)]
-end
+koala_lines = Cycler.new(File.read("koalas.txt").split("\n"))
 
 irc = Net::YAIL.new(
   :address   => server_host,
@@ -27,7 +38,7 @@ irc.on_msg do |event|
   if event.pm?
   elsif event.channel
     if event.message.match(%r(k.*o.*a.*l.*a)i)
-      irc.msg(event.channel, random_line)
+      irc.msg(event.channel, koala_lines.next)
     end
   else
     puts "unhandled message (server?)"
