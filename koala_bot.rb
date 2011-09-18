c19d8413e206236791282c032041baa3edfe2ecf
Christian von Kleist <cvonkleist@gmail.com>
2011-09-17 20:35:53 -0400

    randomize cycler, get rid of some code for events

--- koala_bot.rb
+++ koala_bot.rb
@@ -3,16 +3,13 @@ require "net/yail"
 
 class Cycler
   def initialize(elements)
-    raise "elements empty" if elements.empty?
     @elements = elements
-    @idx_max  = elements.length - 1
-    @idx      = 0
+    @random_elements_pool = []
   end
 
   def next
-    r = @elements[@idx]
-    @idx = (@idx == @idx_max) ? 0 : (@idx + 1)
-    r
+    @random_elements_pool = @elements.clone if @random_elements_pool.empty?
+    @random_elements_pool.delete_at(rand(@random_elements_pool.count))
   end
 end
 
@@ -35,13 +32,8 @@ irc.on_welcome do |event|
 end
 
 irc.on_msg do |event|
-  if event.pm?
-  elsif event.channel
-    if event.message.match(%r(k.*o.*a.*l.*a)i)
-      irc.msg(event.channel, koala_lines.next)
-    end
-  else
-    puts "unhandled event: #{event.inspect}"
+  if event.channel && event.message.match(%r(k.*o.*a.*l.*a)i)
+    irc.msg(event.channel, koala_lines.next)
   end
 end
 
